94290f7284bafad691c422670d58ca56965f4855
roberto2002-03-12 <robertog-18@hotmail.com>
2024-03-15 11:30:25 -0500

    run preview on docker container

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 4173,
+    strictPort: true,
+    host: true
+  },
   server: {
     host: true,
-    strictPort: true,
-    port: 4173
   }
 })
